825cb6c731900d8d3818607494ef263196490fb9
sqh <531687309@qq.com>
2020-04-22 11:53:28 +0800

    "初始化"

---
+++ src/main/java/com/ltc/intelljcp/IntelljcpApplication.java
@@ -0,0 +1,13 @@
+package com.ltc.intelljcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelljcpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelljcpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ltc/intelljcp/IntelljcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltc.intelljcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelljcpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
